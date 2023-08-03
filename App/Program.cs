2d9a2b2c951d0232581204e61c76881ebaeb205f
bbalakriz <37283315+bbalakriz@users.noreply.github.com>
2023-08-03 08:26:44 +0000

    added console app logic and dockerfile

---
+++ App/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+var counter = 0;
+var max = 100;
+while (max is -1 || counter < max)
+{
+    Console.WriteLine($"Counter: {++counter}");
+    await Task.Delay(TimeSpan.FromMilliseconds(2_000));
+}
