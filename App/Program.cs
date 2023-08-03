79ebfcd87580969b75145b376d2dba0f8a3e9083
bbalakriz <37283315+bbalakriz@users.noreply.github.com>
2023-08-03 10:33:03 +0000

    "updated project"

--- App/Program.cs
+++ App/Program.cs
@@ -1,8 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var counter = 0;
-var max = 100;
-while (max is -1 || counter < max)
-{
-    Console.WriteLine($"Wait state: {++counter}");
-    await Task.Delay(TimeSpan.FromMilliseconds(2_000));
-}
+Console.WriteLine("Hello, World!");
